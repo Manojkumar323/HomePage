17b353b9b3be6762e2db16308a368d5863ea8fca
ManojKumar <Manojkumar850876@gmail.com>
2024-12-06 17:57:05 +0530

    Home

--- Front/src/components/Footer/Footer.jsx
+++ Front/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className=" text-white py-8">
+    <footer className="text-white py-8">
       <div className="container mx-auto px-4">
         {/* Footer Content */}
         <div className="flex flex-col sm:flex-row justify-between items-center mb-6">
@@ -15,26 +15,32 @@ const Footer = () => {
               alt="Logo"
               className="w-13 h-20"
             />
-   
           </div>
 
           {/* Contact Section */}
           <div className="text-center sm:text-right mb-4 sm:mb-0">
-            <p>
+            <p className="text-sm sm:text-base md:text-lg font-normal">
               Contact:{" "}
-              <a href="mailto:your-email@example.com" className="text-blue-400 hover:text-blue-600  Atag">
-              yesuraj88@gmail.com
+              <a
+                href="mailto:your-email@example.com"
+                className="text-blue-400 hover:text-blue-600 Atag"
+              >
+                yesuraj88@gmail.com
               </a>
             </p>
           </div>
         </div>
 
         {/* Developer Credit */}
-        <div className="text-center text-sm">
+        <div className="text-center text-sm sm:text-base md:text-lg font-light">
           <p>
             Website developed by{" "}
-            <a href="https://www.seelangraphics.com/" target ="_blank" className="text-blue-400 hover:text-blue-600 Atag">
-            Seelan graphic compass pvt ltd
+            <a
+              href="https://www.seelangraphics.com/"
+              target="_blank"
+              className="text-blue-400 hover:text-blue-600 Atag"
+            >
+              Seelan graphic compass pvt ltd
             </a>
           </p>
           <p>Copyrights © 2024 TinyKarts. All Rights Reserved.</p>
